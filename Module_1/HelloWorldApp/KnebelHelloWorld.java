63f242c1f40b6d8780d7914a3dddd8a03c8718bb
KnebelZachary <110074104+KnebelZachary@users.noreply.github.com>
2024-08-31 23:13:08 -0400

    Added Assignment 1.2 KnebelHelloWorld.java

---
+++ Module_1/HelloWorldApp/KnebelHelloWorld.java
@@ -0,0 +1,9 @@
+public class KnebelHelloWorld {
+    public static void myName(String firstName, String lastName) {
+        System.out.printf("Hello World from %s %s.", firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        myName("Zachary", "Knebel");
+    }
+}
